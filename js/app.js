bbdd6900c094d42b8a3bb5ea112f7a89905082d6
MartaKossowska <martakossowska93@gmail.com>
2017-10-21 14:47:02 +0200

    modyfying app.js

---
+++ js/app.js
@@ -0,0 +1,29 @@
+//console.log("działa!");
+//
+//var name = "Marta";
+//console.log(name);
+//
+//var imiona = ['Monika', 'Marta', 'Paulina', 'Marysia'];
+//console.log(imiona[1]);
+//
+//var numberOne = 10;
+//
+//var numberTwo = 6;
+//
+//if (numberOne>numberTwo) {
+//    console.log('Hurra!');
+//} else {
+//        
+//    }
+//
+//
+//for (var i = 0; i < imiona.length; i++) {
+//    console.log(imiona[i]);
+//}
+
+//console.log('działa');
+//
+////DOM Api
+//
+//var ppp = document.querySelector(".jumbotron");
+//ppp.style.border = '3px solid red';
